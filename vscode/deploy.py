96b6cd29bef6b56df985faceddb8aabc34b30afa
XiaoxingChen <chen-xx@zju.edu.cn>
2020-10-21 22:46:08 +0800

    Add: vscode snippets auto deploy

---
+++ vscode/deploy.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import os
+import sys
+import shutil
+
+class Directory():
+    def __init__(self, argv0):
+        if sys.platform == 'linux':
+            self.vscode_home = os.path.expanduser('~/.config/Code')
+        else:
+            raise RuntimeError("Platform doesn't support!")
+        self.vscode_snippet_folder = os.path.join(self.vscode_home, 'User', 'snippets')
+        self.script_folder = os.path.dirname(os.path.realpath(argv0))
+        self.repo_snippets_folder = os.path.join(self.script_folder, 'snippets')
+
+if __name__ == "__main__": 
+    dirs = Directory(sys.argv[0])
+    for filename in os.listdir(dirs.repo_snippets_folder):
+        if os.path.splitext(filename)[1] == '.json':
+            shutil.copy(os.path.join(dirs.repo_snippets_folder, filename), dirs.vscode_snippet_folder)
+
+    print('done!')
+
