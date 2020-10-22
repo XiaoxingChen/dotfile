f16ae9aced3a37ce8bc5b6b86429d6d9f98ebf02
chen-xx@zju.edu.cn <chen-xx@zju.edu.cn>
2020-10-22 16:38:38 +0800

    Modify: windows support

--- vscode/deploy.py
+++ vscode/deploy.py
@@ -7,6 +7,8 @@ class Directory():
     def __init__(self, argv0):
         if sys.platform == 'linux':
             self.vscode_home = os.path.expanduser('~/.config/Code')
+        elif sys.platform == 'win32':
+            self.vscode_home = os.path.join(os.environ['USERPROFILE'], 'AppData', 'Roaming', 'Code')
         else:
             raise RuntimeError("Platform doesn't support!")
         self.vscode_user_folder = os.path.join(self.vscode_home, 'User')
@@ -14,7 +16,7 @@ class Directory():
         self.script_folder = os.path.dirname(os.path.realpath(argv0))
         self.repo_snippets_folder = os.path.join(self.script_folder, 'snippets')
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     dirs = Directory(sys.argv[0])
 
     shutil.copy(os.path.join(dirs.script_folder, 'keybindings.json'), dirs.vscode_user_folder)
