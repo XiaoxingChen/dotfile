22355550a27e0ec13bf7a386ea98a77395cd5810
XiaoxingChen <chen-xx@zju.edu.cn>
2020-12-20 17:53:22 +0800

    Modify: change alt+tab shortcut

--- gnome/deploy.py
+++ gnome/deploy.py
@@ -4,3 +4,5 @@ import os
 if __name__ == "__main__":
     os.system("gsettings set org.gnome.mutter workspaces-only-on-primary false")
     os.system("gsettings set org.gnome.shell.app-switcher current-workspace-only true")
+    os.system("gsettings set org.gnome.desktop.wm.keybindings switch-applications \"['<Super>Tab']\"")
+    os.system("gsettings set org.gnome.desktop.wm.keybindings switch-windows \"['<Alt>Tab']\"")
