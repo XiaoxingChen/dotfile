f4f2d3245f865c0e8e6fbbc8cb5a1aeecfbb4ae4
XiaoxingChen <chen-xx@zju.edu.cn>
2021-05-10 21:18:43 +0800

    Modify: update vscode script.

--- vscode/deploy.py
+++ vscode/deploy.py
@@ -23,15 +23,32 @@ class Directory():
         self.vscode_local_setting_file = os.path.join(self.script_folder, 'settings.json')
         self.vscode_system_setting_file = os.path.join(self.vscode_user_folder, 'settings.json')
 
-if __name__ == "__main__":
-    dirs = Directory()
+extension_list = [
+    'eamodio.gitlens',
+    'hediet.vscode-drawio',
+    'LeetCode.vscode-leetcode',
+    'ms-python.python',
+    'ms-vscode-remote.remote-ssh',
+    'ms-vscode-remote.remote-ssh-edit',
+    'ms-vscode.cpptools',
+    'redhat.java',
+    'ritwickdey.LiveServer',
+    'streetsidesoftware.code-spell-checker',
+    'twxs.cmake']
 
-    # overwrite keybindings.json
-    shutil.copy(os.path.join(dirs.script_folder, 'keybindings.json'), dirs.vscode_user_folder)
+def installExtensions(ext_list):
+    for extension in ext_list:
+        os.system('code --install-extension ' + extension)
 
+def updateSettingFile(dirs):
+    setting_dict = dict()
     # incrementally modify settings.json
-    with open(dirs.vscode_system_setting_file) as f:
-        setting_dict = json.load(f)
+    if os.path.isfile(dirs.vscode_system_setting_file):
+        with open(dirs.vscode_system_setting_file) as f:
+            setting_dict = json.load(f)
+
+    os.makedirs(dirs.vscode_user_folder, exist_ok=True)
+
     with open(dirs.vscode_local_setting_file) as f:
         setting_dict_patch = json.load(f)
 
@@ -40,10 +57,25 @@ if __name__ == "__main__":
     with open(dirs.vscode_system_setting_file, 'w') as f:
         json.dump(setting_dict, f, indent=2)
 
+def updateKeyBindings(dirs):
+    # overwrite keybindings.json
+    if not os.path.isdir(dirs.vscode_user_folder):
+        os.makedirs(dirs.vscode_user_folder, exist_ok=True)
+    shutil.copy(os.path.join(dirs.script_folder, 'keybindings.json'), dirs.vscode_user_folder)
+
+def updateSnippets(dirs):
     # overwrite snippets folder
+    os.makedirs(dirs.repo_snippets_folder, exist_ok=True)
     for filename in os.listdir(dirs.repo_snippets_folder):
         if os.path.splitext(filename)[1] == '.json':
             shutil.copy(os.path.join(dirs.repo_snippets_folder, filename), dirs.vscode_snippet_folder)
+
+if __name__ == "__main__":
+    dirs = Directory()
+    updateSettingFile(dirs)
+    updateKeyBindings(dirs)
+    updateSnippets(dirs)
+    installExtensions(extension_list)
 
     print('done!')
 
