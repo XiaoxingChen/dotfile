31782acb3842d3777c77343d523cce9f1f3b3e80
XiaoxingChen <chen-xx@zju.edu.cn>
2021-06-07 08:11:35 +0800

    Modify: fix bug in macos deploy.

--- macos/deploy.py
+++ macos/deploy.py
@@ -1,29 +1,41 @@
+#!/usr/bin/python3
+import os
 import sys
 
-def setGlobalIgnore():
-    global_ignore_path = None
-    if sys.platform in {'darwin', 'linux'}:
-        global_ignore_path = os.path.join(os.environ['HOME'], '.gitignore_global')
-    with open(global_ignore_path, 'w') as f:
-        f.write('/.vscode/\n')
-
-    os.system('git config --global core.excludesfile {}'.format(global_ignore_path))
-
-def setGitAutoComplete():
-    if sys.platform != 'darwin':
-        return
-
-    sh_func = """
-if [ -f `brew --prefix`/etc/bash_completion.d/git-completion.bash ]; then
-  . `brew --prefix`/etc/bash_completion.d/git-completion.bash
-fi
-"""
+def addBashOptions(bash_func):
     bash_profile_path = os.path.join(os.environ['HOME'], '.bash_profile')
     with open(bash_profile_path, 'r') as f:
-        if sh_func in f.read():
-            print('git auto complete already configured')
+        if bash_func in f.read():
+            print('option already configured')
             return
 
-    os.system('brew install git bash-completion')
     with open(bash_profile_path, 'a') as f:
-        f.write(sh_func)
+        f.write(bash_func)
+
+def setBashColor():
+    # Reference:
+    # https://stackoverflow.com/questions/1550288/os-x-terminal-colors
+    sh_func = """
+export CLICOLOR=1
+export LSCOLORS=GxFxCxDxBxegedabagaced
+"""
+    print("set bash color")
+    addBashOptions(sh_func)
+
+def setBashComplete():
+    # Reference:
+    # https://itnext.io/programmable-completion-for-bash-on-macos-f81a0103080b
+    sh_func = """
+export BASH_COMPLETION_COMPAT_DIR="/usr/local/etc/bash_completion.d"
+[[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"
+"""
+
+    print("set bash complete")
+    addBashOptions(sh_func)
+
+if __name__ == "__main__":
+    if sys.platform != 'darwin':
+        print('Current OS is not macosx. Quit.')
+        quit()
+    setBashColor()
+    setBashComplete()
