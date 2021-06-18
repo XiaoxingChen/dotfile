cb689837893d54500c00a61dfd772d3cf142d09e
XiaoxingChen <chen-xx@zju.edu.cn>
2021-06-18 23:29:25 +0800

    Modify: optimize codec script.

--- scripts/diff_codec.py
+++ scripts/diff_codec.py
@@ -3,10 +3,15 @@ import base64
 import sys
 import os
 
+def encSuffix():
+    return 'enc'
+
 def codec(input_filename, encode=True):
     splitext = os.path.splitext(input_filename)
-    suffix = '_enc' if encode else '_dec'
-    output_filename = splitext[0] + suffix + splitext[1]
+    if encode:
+        output_filename = splitext[0] + '_' + encSuffix() + splitext[1]
+    else:
+        output_filename = splitext[0][:-len(encSuffix())] + splitext[1]
     with open(input_filename, 'r') as f_in:
         if encode:
             out = base64.b32encode(bytes(f_in.read(), 'utf-8'))
@@ -16,10 +21,16 @@ def codec(input_filename, encode=True):
             f_out.write(out.decode())
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("usage: differ_codec.py [enc|dec] input.txt")
+    if len(sys.argv) != 2:
+        print("usage: differ_codec.py input.txt")
         quit()
 
-    mode, input_filename = sys.argv[1:]
-    encode = True if mode == 'enc' else False
+    input_filename = sys.argv[1]
+    encode = True if (os.path.splitext(input_filename)[0].split('_')[-1] != encSuffix()) else False
+    if encode:
+        print("Encode")
+    else:
+        print("Decode")
     codec(input_filename, encode)
+
+    print("Done")
